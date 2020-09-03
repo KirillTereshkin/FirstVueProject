7c4fb55e8a1cd2d28951fdcc2d1a930f5d4be964
Kiruha Tereshkin <kirill.tereshkin@exactprosystems.com>
2020-09-03 23:02:24 +0300

    #2 Add Navbar toggling

---
+++ src/components/app/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{open: isOpen}">
+    <router-link
+        v-for="(link, index) in links"
+        :key="index"
+        tag="li"
+        active-class="active"
+        :to="link.href"
+        :exact="link.exact"
+        >
+      <a href='#' class="waves-effect waves-orange pointer">{{link.name}}</a> 
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        isOpen: {
+            type: Boolean,
+            required: true,
+        }
+    },
+    data: () => ({
+        links: [
+            { name: "Счет", href: "/", exact: true },
+            { name: "История", href: "/history" },
+            { name: "Планирование", href: "/planning" },
+            { name: "Новая запись", href: "/records" },
+            { name: "Категории", href: "/categories" },
+            ]
+    }),
+}
+</script>
