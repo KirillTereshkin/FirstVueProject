03e9de5f54eae923409b114464170d356a23469d
Kiruha Tereshkin <tereshkinkirill1995@gmail.com>
2020-09-14 00:45:25 +0300

    #4 Added Login and register components v-model and login fields validations

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,26 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
-        <input id="email" type="text" class="validate" />
+        <input id="email" 
+               type="text" 
+               class="validate" 
+               :class="{invalid: emailRequiredValidation || emailTemplateValidation}" 
+               v-model="email"/>
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small v-if="emailRequiredValidation" class="helper-text invalid">Введите email</small>
+        <small v-else-if="emailTemplateValidation" class="helper-text invalid">Введите корректный email</small>
       </div>
       <div class="input-field">
-        <input id="password" type="password" class="validate" />
+        <input id="password" 
+               type="password" 
+               class="validate" 
+               :class="{invalid: passwordRequiredValidation || passwordMinLengthValidation}"
+               v-model="password"/>
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small v-if="passwordRequiredValidation" class="helper-text invalid">Введите пароль</small>
+        <small v-else-if="passwordMinLengthValidation" class="helper-text invalid">Пароль должен быть длиннее {{minLengthPassword}} символов</small>
       </div>
     </div>
     <div class="card-action">
@@ -23,13 +33,60 @@
 
       <p class="center">
         Нет аккаунта?
-        <a href="/">Зарегистрироваться</a>
+        <router-link to="/register">Зарегистрироваться</router-link>
       </p>
     </div>
   </form>
 </template>
 <script>
-export default {
+import { email, required, minLength } from 'vuelidate/lib/validators'
 
+export default {
+  data: () => ({
+    email: "",
+    password: "",
+    minLengthPassword: 10,
+  }),
+  computed: {
+    emailRequiredValidation(){
+      return this.$v.email.$dirty && !this.$v.email.required;
+    },
+    emailTemplateValidation(){
+      return this.$v.email.$dirty && !this.$v.email.email;
+    },
+    passwordRequiredValidation(){
+      return this.$v.password.$dirty && !this.$v.password.required;    
+    },
+    passwordMinLengthValidation(){
+      return this.$v.password.$dirty && !this.$v.password.minLength;
+    }
+  },
+  validations(){
+    return {
+      email: {
+        email,
+        required
+      },
+      password:{
+        minLength: minLength(this.minLengthPassword),
+        required
+      }
+    }
+  },
+  methods: {
+    submitHandler(){
+      if(this.$v.$invalid){
+        this.$v.$touch();
+        return
+      }
+
+      const formData = {
+        email: this.email,
+        password: this.password,
+      }
+
+      this.$router.push("/");
+    }
+  }
 }
 </script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,25 +1,25 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
-        <input id="email" type="text" />
+        <input id="email" type="text" v-model="email"/>
         <label for="email">Email</label>
         <small class="helper-text invalid">Email</small>
       </div>
       <div class="input-field">
-        <input id="password" type="password" class="validate" />
+        <input id="password" type="password" class="validate" v-model="password"/>
         <label for="password">Пароль</label>
         <small class="helper-text invalid">Password</small>
       </div>
       <div class="input-field">
-        <input id="name" type="text" class="validate" />
+        <input id="name" type="text" class="validate" v-model="name"/>
         <label for="name">Имя</label>
         <small class="helper-text invalid">Name</small>
       </div>
       <p>
         <label>
-          <input type="checkbox" />
+          <input type="checkbox" v-model="isAgree"/>
           <span>С правилами согласен</span>
         </label>
       </p>
@@ -34,8 +34,23 @@
 
       <p class="center">
         Уже есть аккаунт?
-        <a href="/">Войти!</a>
+        <router-link to="/login">Войти!</router-link>
       </p>
     </div>
   </form>
 </template>
+<script>
+export default {
+  data: ()=>({
+    email: "",
+    password: "",
+    name: "",
+    isAgree: false,
+  }),
+  methods: {
+    submitHandler(){
+      console.log(this.$data)
+    }
+  }
+}
+</script>
